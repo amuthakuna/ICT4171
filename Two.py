da43e50c919ba9696dd8fb446a96e845cf702b6f
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-10 19:06:23 +0530

    Add files via upload
    CollectionOfValuesIntro, RandomNumber two mehods, Boolean isinstance() and floor division
    Though I expect the rounded value its not supporting need to check

---
+++ Two.py
@@ -0,0 +1,38 @@
+'''
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+'''
+
+'''
+import random
+print(random.randrange(1, 10))
+'''
+
+'''
+import random as r
+print(r.randrange(10, 100))
+'''
+
+#print(bool(5>1))
+#print(bool(5<1))
+#print(bool("Hello"))
+#print(bool(""))
+#print(bool(456))   #twice 
+#print(bool(0))
+#print(bool(["apple", "cherry", "banana"]))
+#print(bool([]))
+#print(bool({}))
+
+
+'''
+x = 200
+print(isinstance(x, int))
+'''
+
+x=11/3
+print(x)
+
