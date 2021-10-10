aa0eeabe5d94fa487d6e1195b6eb345d70c25efc
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-10 18:23:24 +0530

    Add files via upload
    Comment, Print, variables' print and Casting

---
+++ one.py
@@ -0,0 +1,72 @@
+'''
+print("Hello, World!")
+print("Hello\nWorld")
+'''
+
+'''
+x = 10
+print(x)
+'''
+
+'''
+y = "Priya"
+print("Name is ",y) 
+print("Name is "+y) 
+'''
+
+'''
+x = 5
+y = 10
+print(x + y)
+print(x , y)
+'''
+
+'''
+x = 10
+y = 'John'
+z=1.5
+print(type(x))
+print(type(y))
+print(type(z))
+'''
+
+#casting
+'''
+x=10	      # x will be 10
+y = str(x)    # y will be '10'    
+z = float(x)  # z will be 10.0
+
+print(x)
+print(y)
+print(z)
+
+'''
+
+'''
+x, y, z = 10,20,30
+print(x)
+print(y)
+print(z)
+'''
+
+x = y = z = 10
+
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
