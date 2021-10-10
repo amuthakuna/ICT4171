f18ce46233b90c15f5eee3fce6b154d51be884a9
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-10 19:40:17 +0530

    Add files via upload
    String is an array so looping, String length and return range of characters called as slicing

---
+++ Four.py
@@ -0,0 +1,21 @@
+'''
+a = "Hello, World!"
+print(a[1])
+'''
+
+'''
+for x in "banana":
+  print(x)
+'''
+
+'''
+a = "Hello, World!"
+print(len(a))
+'''
+
+'''
+b = "Hello, World!"
+print(b[2:5]) 	#Get the characters from position 2 to position 5 (not included):
+print(b[:5]) 	#Slice From the Start
+print(b[2:])	#Slice To the End
+'''
