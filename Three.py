583a0e2ff0f2af7bbd97ef21f8f3ccbdfb037789
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-10 19:24:47 +0530

    Add files via upload
    if, elif, else and also nestedconditional statements

---
+++ Three.py
@@ -0,0 +1,92 @@
+'''
+a = 10
+b = 20
+
+if b > a:
+	print("b is greater than a")
+elif a == b:
+	print("a and b are equal")
+else:
+	print("a is less than b")
+
+'''
+
+'''
+a = 10
+b = 20
+if b > a: print("b is greater than a")
+elif a == b: print("a and b are equal")
+else: print("a is less than b")
+
+'''
+
+'''
+a = 10
+b = 20
+print("b is greater than a") if b > a else print("a is less than b")
+'''
+
+'''
+a = 30
+b = 30
+
+print(b) if b > a else print("=") if a == b else print(a)
+
+'''
+
+'''
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+'''
+
+'''
+g = "female"
+a=30
+
+if g == "female":
+  if a > 20:
+    print("Female above 20")
+  else:
+    print("Female not above 20.")
+else:
+	print("Male not above 20.")
+	
+'''
+
+'''
+g = "female"
+a=30
+
+if (g == "female"):
+    print("Female", end="")
+    if a > 20:
+        print("above 20")
+    else:
+        print("Female not above 20.")
+else:
+    print("Male not above 20.")
+	
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
