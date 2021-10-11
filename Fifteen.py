ec63f52ad3e9a8bedf05cfc95d25363629e2ff5a
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 18:43:31 +0530

    Add files via upload
    reverse, sort
    sort is case sensitive

---
+++ Fifteen.py
@@ -0,0 +1,55 @@
+'''
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
+'''
+
+'''
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+'''
+
+'''
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+'''
+
+'''
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+'''
+
+#notworking the follwing two
+'''
+def myfunc(n):
+  return abs(n - 50)
+'''
+
+'''
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+'''
+
+'''
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+'''
+'''
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+'''
+
+
+
+
+
+
+
