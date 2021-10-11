958f32366aad398a46472fa1e19698880b176fd5
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 13:14:14 +0530

    Add files via upload
    input and several ways to deal function

---
+++ Ten.py
@@ -0,0 +1,64 @@
+"""
+a=input()
+print(a)
+
+"""
+
+"""
+a=input("Enter username:")
+print(a)
+"""
+
+"""
+def my_function():
+  print("Hello from a function")
+
+my_function()
+"""
+
+"""
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+"""
+
+"""
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+"""
+
+"""
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+"""
+
+"""
+def my_function(country = "Sri Lanka"):
+  print("I am from " + country)
+
+my_function("India")
+my_function()
+
+"""
+def my_function(x):
+  return 5 * x
+
+
+
+
+
+
+
+
+
+
+
+
+
