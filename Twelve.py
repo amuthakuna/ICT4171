a79dedfffa95f86323127835e44a1b473a4160ef
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 17:20:53 +0530

    Add files via upload
    list introduction

---
+++ Twelve.py
@@ -0,0 +1,41 @@
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[0])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[-1])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[2:5])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[:4])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[2:])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(thislist[-4:-1])
+'''
+
+'''
+thislist = ["apple", "banana", "cherry","orange", "kiwi", "melon"]
+print(len(thislist))
+
+
+'''
