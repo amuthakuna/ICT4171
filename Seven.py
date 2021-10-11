560126a5ef161d10be66d36e349207be9f388111
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 11:31:12 +0530

    Add files via upload
    String is an array so looping
    in keyword to check string availability and ifelse usage
    escape character backslash

---
+++ Seven.py
@@ -0,0 +1,79 @@
+"""
+for x in "fruit":
+  print(x)
+  
+"""
+
+"""
+for x in "banana":
+  print(x, end="")
+
+"""
+
+"""
+for x in "banana":
+  print(x)
+
+"""
+
+"""
+txt = "The best things in life are free!"
+print("free" in txt)
+
+"""
+
+"""
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+"""
+
+"""
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+"""
+"""
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("Yes, 'expensive' is NOT present.")
+
+"""
+"""  
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+"""
+"""
+txt = "We are the so-called 'Vikings' from the north."
+print(txt)
+"""
+"""
+txt = 'We are the so-called "Vikings" from the north.'
+print(txt)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
