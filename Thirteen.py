fbc6a944da727d775c133168425de67249a190c6
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 18:05:31 +0530

    Add files via upload
    list 2 ways declaration
    list, in keyword and conditional statement
    changing or replacing list items

---
+++ Thirteen.py
@@ -0,0 +1,58 @@
+'''
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+'''
+
+'''
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+'''
+
+
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+'''
+
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[0] = "blackcurrant"
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+'''
+
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[3:4] = ["blackcurrant", "watermelon"]
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
