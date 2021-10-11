9d1650733846f6131999844dbaff9bbe9c9fb904
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 12:01:33 +0530

    Add files via upload
    String functions upper, lower, strip, replace, split and count
    String function find need more clarification on their index

---
+++ Eight.py
@@ -0,0 +1,78 @@
+"""
+a = "Hello, World!"
+print(a.upper())
+"""
+
+"""
+a = "Hello, World!"
+print(a.lower())
+
+"""
+
+"""
+a = "Hello, World!"
+print(a.strip())
+
+"""
+
+"""
+a = "Hello, World!"
+print(a.replace("H", "J"))
+"""
+
+"""
+a = "Hello, World!"
+print(a.split(","))
+
+"""
+
+"""
+txt = "I love apples, apple are my favorite fruit"
+print(txt.count("apple"))
+"""
+
+"""
+txt = "I love apples, apple are my favorite fruit"
+print(txt.count("apple", 15, 24))
+"""
+
+"""
+txt = "Hello, welcome to my world."
+print(txt.find("e"))
+"""
+
+"""
+txt = "Hello, welcome to my world."
+print(txt.find("e", 5, 10))
+"""
+
+"""
+txt = "Hello, welcome to my world."
+print(txt.find("welcome"))
+"""
+
+"""
+txt = "Hello, welcome to my world."
+print(txt.find("Hi"))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
