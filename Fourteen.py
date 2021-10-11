8441730808edf26efe7c0980d740d096e6e0efb7
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-11 18:29:35 +0530

    Add files via upload
    insert, append, extend list and other tuple or set or dictionary
    remove, pop, del, clear
    count, index

---
+++ Fourteen.py
@@ -0,0 +1,136 @@
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist.insert(2, "watermelon")
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist.append("orange")
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+'''
+
+''''
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+'''
+'''
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)	
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()	
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+del thislist
+print(thislist)
+'''
+
+'''
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+'''
+
+'''
+points = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x = points.count(9)
+print(x)
+'''
+
+'''
+points = [4, 55, 64, 32, 16, 32]
+x = points.index(32)
+print(x)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
