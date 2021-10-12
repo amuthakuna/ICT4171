eeb2506cdf3477162000e17fd053660983b0a9c8
Amutha Kunarathinam <67719052+amuthakuna@users.noreply.github.com>
2021-10-12 08:02:48 +0530

    Add files via upload
    forloop and dictinary
    d.keys(),d.values() and d.items()

---
+++ twenty.py
@@ -0,0 +1,73 @@
+'''
+d={
+'name':'Jhon',
+'age':24,
+'gender':'male'
+}
+
+for x in d:
+  print(x)
+print()
+'''
+
+'''
+d={
+'name':'Jhon',
+'age':24,
+'gender':'male'
+}
+for x in d:
+  print(d[x])
+print()
+'''
+
+'''
+d={
+'name':'Jhon',
+'age':24,
+'gender':'male'
+} 
+for x in d.values():
+  print(x)
+print()
+
+'''
+'''
+d={
+'name':'Jhon',
+'age':24,
+'gender':'male'
+} 
+for x in d.keys():
+  print(x)
+print()
+'''
+
+'''
+
+d={
+'name':'Jhon',
+'age':24,
+'gender':'male'
+}
+
+for x, y in d.items():
+  print(x, y)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
